78a43ae3495a8c4075f592d5c09ad364dbae3ac2
dodo <saad9191h@gmail.com>
2022-05-29 08:36:03 -0400

    added a function create grid according to size input

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-let size = 8;
-let sizeSq = size * size;
+let containerDimension = 350; //600px
+let squareBorder = 1; //1px border
 
+
+//reference to grid container. Needed for createGrid function
 const gridContainer = document.querySelector('#grid-container');
 
 
-for (let i = 0; i < sizeSq; i++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('grid-square');
+//taking input of grid size
+let gridSize = +prompt("Enter size of grid(max 64");
 
-    gridContainer.appendChild(gridSquare);
-}
+createGrid(gridSize);
 
 
 const gridSquare = document.querySelectorAll('.grid-square');
-
-
 gridSquare.forEach((square) => {
 
     square.addEventListener('mouseover', () => {
-        square.style.cssText = "background-color: black";
+        square.style['background-color'] = "black";
     })  
 });
 
@@ -26,4 +24,21 @@ gridSquare.forEach((square) => {
 
 
 
-// create function that creates a box (in memory) then appends to gridContainer
+
+function createGrid(size) {
+    let sizeSq = size ** 2;
+
+
+    //calcuating square width and height accroding to grid size
+    let squareDimension = ( containerDimension - (2*size * squareBorder) ) / size;
+
+    //this loop creates a square in memory according to the calcuated dimensions, and then appends that square to the grid-container
+    for (let i = 0; i < sizeSq; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
+        
+        gridSquare.style.width = squareDimension + 'px'; 
+        gridSquare.style.height = squareDimension + 'px';
+        gridContainer.appendChild(gridSquare);
+    }
+}
