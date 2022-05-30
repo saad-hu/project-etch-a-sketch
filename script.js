c15da2adc81a95f36f117fed77dae1642507ed69
dodo <saad9191h@gmail.com>
2022-05-30 14:21:31 -0400

    fixed prompt cancel and escape issue and added a few more comments

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const containerDimension = 600; //600px
 const squareBorder = 1; //1px border
-const defaultGridSize = 16
+const defaultGridSize = 16;
 
 const gridContainer = document.querySelector('#grid-container'); //reference to grid container. Needed for createGrid function
 const buttonGridSize = document.querySelector('#button-grid-size');
@@ -10,14 +10,11 @@ window.onload = function() {
     createGrid(defaultGridSize);
 }
 
+//adding an event to change grid button
 buttonGridSize.addEventListener('click', () => {
     let size = takeGridSize();
     createGrid(size);
 });
-
-
-
-
 
 
 
@@ -26,13 +23,16 @@ function takeGridSize() {
     
     let correctInput, gridSize;
     do {
-        gridSize = +prompt("Enter size of new grid(1-64)");
-        if(gridSize >= 1 && gridSize <= 64) correctInput = true;
+        gridSize = prompt("Enter size of new grid(1-64)");
+        if(gridSize == null) return defaultGridSize;    //when the user presses cancel or escape
+
+        else if(gridSize >= 1 && gridSize <= 64) correctInput = true;
 
         else {
             alert("Please enter a valid number.")
             correctInput = false;
         } 
+        console.log(gridSize);
     }while(correctInput == false)
     
     return gridSize;
@@ -52,6 +52,7 @@ function createGrid(size) {
     //calcuating square width and height accroding to grid size
     let squareDimension = ( containerDimension - (2*size * squareBorder) ) / size;
 
+    
     //this loop creates a square in memory according to the calcuated dimensions, and then appends that square to the grid-container
     for (let i = 0; i < sizeSq; i++) {
         const gridSquare = document.createElement('div');
@@ -63,8 +64,8 @@ function createGrid(size) {
     }
 
     //the boxes have been created and appended to the grid. now the grid is visible. 
+
     //this code will add a mouseover eventlistener to each squarebox that has been created. that is, this will activate sketching
-    
     const gridSquare = document.querySelectorAll('.grid-square'); //nodelist of all squares 
 
     gridSquare.forEach((square) => {  //using for each to iterate over each square box and adding eventlistener
